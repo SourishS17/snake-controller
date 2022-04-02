03e8055f77209a5abc601e147d6c61f8f7ac85ad
Sourish <70473280+SourishS17@users.noreply.github.com>
2022-04-02 23:44:08 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ radio.setGroup(1)
 let started = false
 
 // Threshold to determine how much tilt is registered to move
-let threshold = 20
+let threshold = 200
 
 // Keeping track of directions
 let dir = 1
@@ -14,51 +14,68 @@ let dir = 1
 let score = 0
 
 // Checking when to begin game
-while (!started && score === 0) {
+basic.forever(function() {
+    if (!started && score === 0) {
 
-    // Start the game on shake
-    input.onGesture(Gesture.Shake, function() {
-        started = true
-        radio.sendValue("start", 1)
-    })
+        // Start the game on shake
+        input.onGesture(Gesture.Shake, function () {
+            started = true
+            radio.sendValue("start", 1)
+        })
 
-}
+    } else {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    }
+})
 
 
-// Main game loop
-while (started) {
+basic.forever(function() {
 
-    // Keep checking for new movements
-    let dirx = input.acceleration(Dimension.X)
-    let diry = input.acceleration(Dimension.Y)
+    // Main game loop
+    if (started) {
 
-    if (dirx > threshold) {
-        dir = 1
-    } else if (dirx < -threshold) {
-        dir = 3
-    } else if (diry > threshold) {
-        dir = 0
-    } else if (diry < -threshold) {
-        dir = 2
+        // Keep checking for new movements
+        let dirx = input.acceleration(Dimension.X)
+        let diry = input.acceleration(Dimension.Y)
+
+        if (dirx > threshold) {
+            dir = 1
+        } else if (dirx < -threshold) {
+            dir = 3
+        } else if (diry > threshold) {
+            dir = 2
+        } else if (diry < -threshold) {
+            dir = 0
+        }
+
+        radio.sendValue("dir", dir)
+
+        // Checking if the game is over
+        radio.onReceivedValue(function (name: string, value: number) {
+            if (name === "score") {
+                started = false
+            }
+        })
+
     }
 
-    radio.sendValue("dir", dir)
+    
+    // Game ended
+    if (!started && score > 0) {
 
-    // Checking if the game is over
-    radio.onReceivedValue(function (name: string, value: number) {
-        if (name === "score") {
-            started = false
-        }
-    })
-
-}
-
-// Game ended
-if (!started && score > 0) {
-
-    // Display score every 3 seconds
-    basic.showNumber(score)
-    pause(3000)
+        // Display score every 3 seconds
+        basic.showNumber(score)
+        pause(3000)
 
 
-}
+    }
+
+})
+
+
